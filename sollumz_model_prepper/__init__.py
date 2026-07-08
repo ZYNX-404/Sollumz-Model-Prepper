93a1a00f9bac12033177c80398d799e663685f2d
ZYNX-404 <reso.jinx.kj512@gmail.com>
2026-07-08 20:41:08 +0900

    0.1.2 release

--- sollumz_model_prepper/__init__.py
+++ sollumz_model_prepper/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Sollumz Model Prepper",
     "author": "ZYNX-404",
-    "version": (0, 1, 1),
+    "version": (0, 1, 2),
     "blender": (5, 1, 2),
     "category": "Object",
     "description": "Prepare models for Sollumz (GTA V / FiveM) export",
